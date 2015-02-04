0c61a4b88c50edb4dc5c0b0332512dae936ac456
hansdg1 <hansdg1+github@gmail.com>
2015-02-03 23:21:54 -0600

    Corrected, verified w/graded solution.

--- hw1.m
+++ hw1.m
@@ -4,20 +4,18 @@
 %   ECE 648                      %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% The number of bits
-NumBits = 2^4; 
-
-% Input array
-s = [-.89 -.66 -.48 -.23 0 .23 .48 .66 .89];
-
+NumBits = 4; % The number of bits
+Q = 2/2^NumBits;
+% S = [-.89 -.66 -.48 -.23 0 .23 .48 .66 .89]; % Input array
+S = [-.23 -.48 -.66 -.89 0 .23 .48 .66 .89];
 % Scale the array so that floor works properly
-scaled = NumBits .* s;       
+% scaled = NumBits .* s;            %%Don't do this
 
 % Perform midtreader quantization
-midtreader = sign(s) .* (floor(abs(scaled))) .* (1/NumBits);
+midtreader = sign(S) .* floor(abs(S)/Q) * Q;
 
 % Perform midriser quantization
-midriser = sign(s) .* (ceil(abs(scaled))) .* (1/NumBits);
+midriser = sign(S) .* ceil(abs(S)/Q) * Q;
 
 % Print results for each
 fprintf('Mid-Treader: ');
